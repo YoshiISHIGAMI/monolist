eddd37ef442ccedf00b038fb548f0dec7496fc9e
Yoshi Ishigami <yoshi@ibf.sc>
2015-08-19 18:15:00 +0900

    to c9

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,7 @@
 class Item < ActiveRecord::Base
   serialize :raw_info , Hash
 
-  has_many :ownerships , foreign_key: "item_id" , dependent: :destroy
+  has_many :ownerships  , foreign_key: "item_id" , dependent: :destroy
   has_many :users , through: :ownerships
 
   has_many :wants , class_name: "Want" , foreign_key: "item_id"
@@ -9,10 +9,4 @@ class Item < ActiveRecord::Base
   
   has_many :haves , class_name: "Have" , foreign_key: "item_id"
   has_many :have_user , through: :haves , source: :user
-
-  # has_many :wants , class_name: "Want", foreign_key: "user_id", dependent: :destroy
-  # has_many :want_items , through: :wants, source: :item
-  
-  # has_many :haves , class_name: "Have" , foreign_key: "user_id", dependent: :destroy
-  # has_many :have_items , through: :haves , source: :item
 end
